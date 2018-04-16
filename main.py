9578322f2d178f7458a76da85a3b4b540502e42e
AHBruns <alex.h.bruns@gmail.com>
2018-04-16 10:36:20 -0500

    finished

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import requests
 import time
-sc = requests.get('http://www.multpl.com/').text
-data_str = sc.split('<b>Current<span class="currentTitle"> S&amp;P 500 PE Ratio</span>:</b>')[1].split('<div id="timestamp">')[0]
-pe_cur = float(data_str.split('<span class="pos">')[0].strip("\n").strip(" "))
-pe_pt_chg = float(data_str.split('<span class="pos">')[1].split('</span>')[0].split(" ")[0])
-pe_pcnt_chg = float(data_str.split('<span class="pos">')[1].split('</span>')[0].split(" ")[1].strip("(").strip("%)"))
-print(pe_cur, pe_pt_chg, pe_pcnt_chg)
+
+it = time.time()
+if abs((time.time() % 86400) - (it % 86400)) < 120:
+    sc = requests.get('http://www.multpl.com/').text
+    data_str = sc.split('<b>Current<span class="currentTitle"> S&amp;P 500 PE Ratio</span>:</b>')[1].split('<div id="timestamp">')[0]
+    pe_cur = float(data_str.split('<span class="pos">')[0].strip("\n").strip(" "))
+    pe_pt_chg = float(data_str.split('<span class="pos">')[1].split('</span>')[0].split(" ")[0])
+    pe_pcnt_chg = float(data_str.split('<span class="pos">')[1].split('</span>')[0].split(" ")[1].strip("(").strip("%)"))
+    print("PE:", pe_cur)
+    fh = open("data.txt", "a")
+    line = "PE " + str(pe_cur) + " " + str(time.time()) + "\n"
+    fh.write(line)
+    fh.close()
+    print("Data written")
+    time.sleep(125)
